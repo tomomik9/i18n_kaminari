db8dc61407de936882c11ac960d0a99dcce48d97
tomomik9 <tomomi.kanaz@gmail.com>
2019-05-09 18:48:48 +0900

    kaminari実装完了済み&デフォルト件数変更済み

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all.page(params[:page]).per(3)
+    @books = Book.page(params[:page]).per(5)
   end
   # GET /books/1
   # GET /books/1.json
